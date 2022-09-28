2117c386bc4a40bd58aafb3f2700e9d30b44c171
Sam Silverman <mr.samsilverman@gmail.com>
2022-09-28 16:40:58 -0400

    Remove cls creation functions (will be added back in new script)

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,3 @@ from .summed_cosine import summed_cosine_radii, \
                            summed_cosine_arc_length
 from .coordinates import polar_to_cartesian, cartesian_to_polar
 from .curves import self_intersection, offset_curve
-from .cls_creation import create_cls, create_cls_from_csv
