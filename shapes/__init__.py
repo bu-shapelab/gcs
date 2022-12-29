a79e325d3edd9ccb82fdf1e180a8d8d784387463
Sam Silverman <mr.samsilverman@gmail.com>
2022-12-29 11:33:37 -0500

    Remove old code

--- shapes/__init__.py
+++
@@ -1,1 +0,0 @@
-from .cls import CLS
--- utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .coordinates import polar_to_cartesian, cartesian_to_polar
-from .integration import simpsons_rule
-from .polar_curves import offset_curve, self_intersection
-from .summed_cosine import summed_cosine, summed_cosine_arc_length, summed_cosine_scaling_factor
