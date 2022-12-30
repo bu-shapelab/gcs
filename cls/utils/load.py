8b41d12c910d92c2ca26e0448bd92c5c87e294a6
Sam Silverman <mr.samsilverman@gmail.com>
2022-12-30 10:52:39 -0500

    Test cls.load

--- cls/utils/load.py
+++ cls/utils/load.py
@@ -3,22 +3,38 @@ from __future__ import annotations
 from typing import Union, TYPE_CHECKING
 import pandas as pd
 
-from ..cls import CLS
+from cls import CLS
 
 if TYPE_CHECKING:
     from os import PathLike
 
 
-def load(file: Union[str, bytes, PathLike]) -> list[CLS]:
+def load(file: Union[str, bytes, PathLike], verbose: bool = False) -> list[CLS]:
     """TODO
     """
-    csv = pd.read_csv(filepath_or_buffer=file, sep=',', header=0)
+    try:
+        csv = pd.read_csv(filepath_or_buffer=file, sep=',', header=0)
+    except FileNotFoundError:
+        if verbose:
+            print('TODO')
+        return []
+    except pd.errors.EmptyDataError:
+        if verbose:
+            print('TODO')
+        return []
 
     shapes = []
 
     for _, row in csv.iterrows():
         parameters = row.to_dict()
-        shape = CLS(**parameters)
+
+        try:
+            shape = CLS(**parameters)
+        except TypeError:
+            if verbose:
+                print('TODO')
+            return []
+
         shapes.append(shape)
 
     return shapes
