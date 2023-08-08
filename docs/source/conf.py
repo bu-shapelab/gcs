3bac24a62146e4605d0d1aa94a3568b5cdf9066a
Sam Silverman <mr.samsilverman@gmail.com>
2023-08-07 22:45:43 -0400

    Update docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.viewcode',
-    'numpydoc',
 ]
 
 # Make sure the target is unique
@@ -54,7 +53,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
