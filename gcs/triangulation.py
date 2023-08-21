17768535a83cacabe0b26ee20bbfcf073c82f7b4
Sam Silverman <mr.samsilverman@gmail.com>
2023-08-21 11:26:58 -0400

    Triangulate top/base faces accordingly

--- gcs/triangulation.py
+++ gcs/triangulation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 import numpy as np
+import mapbox_earcut as earcut
 
 
 if TYPE_CHECKING:
@@ -30,6 +31,8 @@ def triangulate(shape: gcs.GCS) -> np.ndarray:
     >>> faces = gcs.faces
 
     """
+    triangulate_faces = shape.parameters['triangulate_faces']
+
     vertices = shape.vertices
 
     parameters = shape.parameters
@@ -69,5 +72,27 @@ def triangulate(shape: gcs.GCS) -> np.ndarray:
             upper_triangle_index = lower_triangle_index + \
                 n_vertices_per_step * (n_steps - 1)
             faces[upper_triangle_index, :] = upper_triangle
+
+    if triangulate_faces:
+        base_vertices = vertices[:n_vertices_per_step:, :2]
+        top_vertices = vertices[-n_vertices_per_step:, :2]
+
+        # from mapbox_earcut:
+        # An array of end-indices for each ring (1st ring is outer contour of the polygon).
+        rings = np.array([n_vertices_per_step])
+
+        triangles_indices_base = earcut.triangulate_float32(base_vertices, rings)
+        triangles_indices_top = earcut.triangulate_float32(top_vertices, rings)
+
+        # offset top indices to correct indices
+        triangles_indices_top = triangles_indices_top + (vertices.shape[0] - n_vertices_per_step)
+
+        faces_base = triangles_indices_base.reshape(-1, 3)
+        faces_top = triangles_indices_top.reshape(-1, 3)
+
+        # Flip order of base vertices for outward facing normals
+        faces_base = np.fliplr(m=faces_base)
+
+        faces = np.vstack((faces, faces_base, faces_top))
 
     return faces
